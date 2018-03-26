1e5a5a869e7b71cb49150588efeeac21c07d4e57
Harry Moreno <hmoreno@H9435.ad.hugeinc.com>
2018-03-26 10:09:47 -0400

    Add git aliases from zsh

--- git-aliases.sh
+++ git-aliases.sh
@@ -3,12 +3,15 @@
 # you can load these by adding `source git-aliases.sh` in your .bash_profile for example
 
 : <<'END'
+
 # Query/use custom command for `git`.
 zstyle -s ":vcs_info:git:*:-all-" "command" _omz_git_git_cmd
 : ${_omz_git_git_cmd:=git}
+
 #
 # Functions
 #
+
 # The name of the current branch
 # Back-compatibility wrapper for when this function was defined here in
 # the plugin, before being pulled in to core lib/git.zsh as git_current_branch()
@@ -35,6 +38,7 @@ function work_in_progress() {
     echo "WIP!!"
   fi
 }
+
 END
 
 #
@@ -122,6 +126,7 @@ ggfl() {
 git push --force-with-lease origin "${b:=$1}"
 }
 compdef _git ggf=git-checkout
+
 ggl() {
   if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
     git pull origin "${*}"
@@ -131,6 +136,7 @@ ggl() {
   fi
 }
 compdef _git ggl=git-checkout
+
 ggp() {
   if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
     git push origin "${*}"
@@ -140,6 +146,7 @@ ggp() {
   fi
 }
 compdef _git ggp=git-checkout
+
 ggpnp() {
   if [[ "$#" == 0 ]]; then
     ggl && ggp
@@ -148,17 +155,22 @@ ggpnp() {
   fi
 }
 compdef _git ggpnp=git-checkout
+
 ggu() {
   [[ "$#" != 1 ]] && local b="$(git_current_branch)"
   git pull --rebase origin "${b:=$1}"
 }
 compdef _git ggu=git-checkout
+
 alias ggpur='ggu'
 compdef _git ggpur=git-checkout
+
 alias ggpull='git pull origin $(git_current_branch)'
 compdef _git ggpull=git-checkout
+
 alias ggpush='git push origin $(git_current_branch)'
 compdef _git ggpush=git-checkout
+
 alias ggsup='git branch --set-upstream-to=origin/$(git_current_branch)'
 alias gpsup='git push --set-upstream origin $(git_current_branch)'
 END
